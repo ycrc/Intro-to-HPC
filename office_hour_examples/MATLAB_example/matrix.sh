2430585672ff66f6641c4b7e2a28c5ee2432b118
Aya Nawano <an492@login2.mccleary.ycrc.yale.edu>
2023-10-17 16:33:07 -0400

    Fixed the reservation name

--- office_hour_examples/MATLAB_example/matrix.sh
+++ office_hour_examples/MATLAB_example/matrix.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 #### Run on workshop reservation ###
-#SBATCH --reservation=workshop
+#SBATCH --reservation=ycrc_intro
 
 #### Request 1 CPU #####
-#SBATCH XXXXX 1
+#SBATCH XX 1
 
 #### Request 10 GB of memory per CPU ###
-#SBATCH XXXXX 10G
+#SBATCH XXXXXXXXXXXXX 10G
 
 #### Request 10 minites of run time ###
-#SBATCH XXXXX 10:00
+#SBATCH XX 10:00
 
 #### Set the name of the job as MATLAB_example ####
-#SBATCH XXXXX MATLAB_example
+#SBATCH XX MATLAB_example
 
 #### Provide email for notification of job start/completion ###
 #SBATCH --mail-user=XXXXX
-#SBATCH --mail-type=XXXXX
+#SBATCH --mail-type=XXX
 
 ##### Remove any loaded modules ####
 
--- office_hour_examples/R_example/pscl_example.sh
+++ office_hour_examples/R_example/pscl_example.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 #### Run on workshop reservation ###
-#SBATCH --reservation=workshop
+#SBATCH --reservation=ycrc_intro
 
 #### Request 1 CPU #####
-#SBATCH XXXXX 1
+#SBATCH XX 1
 
 #### Request 10 minites of run time ###
-#SBATCH XXXXX 10:00
+#SBATCH XX 10:00
 
 #### Set the name of the job as PSCL_example ####
-#SBATCH XXXXX PSCL_example
+#SBATCH XX PSCL_example
 
 #### Provide email for notification of job start/completion ###
 #SBATCH --mail-user=XXXXX
-#SBATCH --mail-type=XXXXX
+#SBATCH --mail-type=XXX
 
 ##### Remove any loaded modules ####
 
