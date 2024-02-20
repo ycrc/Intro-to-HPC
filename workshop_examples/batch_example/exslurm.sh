bde273f813dad7f446ad4661b3fa8358757b47ac
Aya Nawano <an492@login1.mccleary.ycrc.yale.edu>
2024-02-20 08:54:21 -0500

    Fixed exslurm.sh

--- workshop_examples/batch_example/exslurm.sh
+++ workshop_examples/batch_example/exslurm.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-####Run on workshop reservation####
+### Run on workshop reservation, remove this line if you are submitting this after the workshop ####
 #SBATCH --reservation=ycrc_intro
 
-###Name your job as MC_test###
+### Run on day partition ###
+#SBATCH -p day 
+
+### Name your job as MC_test ###
 #SBATCH -J XXXXXXX
 
-####request 1G of mem/cpu#####
+### Request 1 GB of mem/cpu ###
 #SBATCH --mem-per-cpu=XG
 
-####Request 10 minutes of run time####
+### Request 10 minutes of run time ###
 #SBATCH -t XX:XX
 
-####provide email for notification of job start/completion####
+### Receive email notifications when this job starts/completes ###
 #SBATCH --mail-type=XXX
 
-#######remove any loaded modules######
+
+
+
+### Remove any loaded modules ###
 module COMMANDTOREMOVEMODULES
 
-####load necessary module to run R###
+### Load R module ###
 INSERT COMMAND TO LOAD MODULE HERE
 
-####execute R script######
+### Execute the R script ###
 Rscript SCRIPTNAME 
