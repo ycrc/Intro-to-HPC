ffd207ccbb8b81b7aa82286a3b9976afdf01757a
Robert Bjornson <robert.bjornson@yale.edu>
2024-07-29 10:48:18 -0400

    added filled in script; reduced iterations

---
+++ workshop_examples/batch_example/exslurm_filled.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+### Run on workshop reservation, remove this line if you are submitting this after the workshop ####
+##SBATCH --reservation=ycrc_intro
+
+### Run on day partition ###
+#SBATCH -p day 
+
+### Name your job as MC_test ###
+#SBATCH -J MC_test 
+
+### Request 1 GB of mem/cpu ###
+#SBATCH --mem-per-cpu=1G
+
+### Request 10 minutes of run time ###
+#SBATCH -t 10:00 
+
+### Receive email notifications when this job starts/completes ###
+#SBATCH --mail-type=all
+
+
+
+
+### Remove any loaded modules ###
+module purge 
+
+### Load R module ###
+module load R
+
+### Execute the R script ###
+Rscript MC.R 
