395d619e5b7b75f83409923586dbbed6b6c8ecac
cvsindelar <charles.sindelar@yale.edu>
2025-04-15 06:47:30 -0400

    Update bwaParallel.sh
    switched the partition to devel

--- workshop_examples/parallel_example/bwaParallel.sh
+++ workshop_examples/parallel_example/bwaParallel.sh
@@ -3,6 +3,7 @@
 # This is the RIGHT way to submit a 4 thread bwa run.  We allocate one task with 4 cpus, and tell bwa to start 4 threads.
 # In a real run, we'd likely use more than 4, perhaps 64.
 
+#SBATCH -p devel
 #SBATCH -c 4
 #SBATCH --mem=16g
 #SBATCH -t 10:00
