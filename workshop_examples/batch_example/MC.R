6900b3abbe4cd0f04ed266338376693e8872ac46
cvsindelar <charles.sindelar@yale.edu>
2025-05-14 15:31:12 -0400

    Update MC.R

--- workshop_examples/batch_example/MC.R
+++ workshop_examples/batch_example/MC.R
@@ -1,6 +1,9 @@
 # Run Monte Carlo simulations to approximate the value of pi 
 runs <-120000000
 within_circle <- 0
+
+message(sprintf("Percent complete: "))
+
 for (x in 1:runs){
     #runif samples from a uniform distribution
     xs <- runif(1)
@@ -11,10 +14,12 @@ for (x in 1:runs){
     }
     pi_estimate <- (sum(within_circle)/x)*4
 
-    if(x %% 120000 == 0) {
-        message(sprintf("%5.1f percent complete", 100*x/runs))
+    if(x %% (runs/1000) == 0) {
+        cat(".")
     }
-
+    if(x %% (runs/100) == 0) {
+        cat(sprintf("%3.f\n", 100*x/runs))
+    }
 }
 print(paste("MC Approximation of Pi =",pi_estimate))
 
